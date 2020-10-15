352b23007ed67c6b0790b9bd3422488606c0eb34
PseudoDevs <revocyberarmy@gmail.com>
2020-10-15 22:18:10 +0800

    Modal for Contacts Done, Chat me pag may mali

---
+++ resources/views/layouts/account-addCustomer-Individual.blade.php
@@ -0,0 +1,20 @@
+<!-- Modal -->
+<div class="modal fade" id="accountAddIndividualCustomer" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Add Customer Individual</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+           @include('layouts.contact-addCustomerIndividual')
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-youtube" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-success">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
